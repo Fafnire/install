433254c58079e3aeca6a13fce477176c21fe2e89
Fafnire <84567173+Fafnire@users.noreply.github.com>
2021-08-03 10:30:10 +0200

    Update installAgent.sh

--- installAgent.sh
+++ installAgent.sh
@@ -31,8 +31,8 @@ apt -y install terraform
 #Install azure-cli
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-#Install maven, unzip and jq
-apt -y install unzip maven jq
+#Install maven and unzip
+apt -y install unzip maven
 
 #Install sonarqube
 cd /home/$5
